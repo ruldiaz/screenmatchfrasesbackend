647d1e21d8e6ebd9a1b80ccab934a0f8ffef0fee
ruldiaz <rulhdiaz@gmail.com>
2025-01-21 10:57:32 -0600

    screenmatch frases backend

---
+++ src/main/java/com/aluracursos/screenmatch_frases/FraseController.java
@@ -0,0 +1,16 @@
+package com.aluracursos.screenmatch_frases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FraseController {
+    @Autowired
+    FraseService servicio;
+
+    @GetMapping("/series/frases")
+    public FraseDTO obtenerFraseAleatoria(){
+        return servicio.obtenerFraseAleatoria();
+    }
+}
---
+++ src/main/java/com/aluracursos/screenmatch_frases/FraseRepository.java
@@ -0,0 +1,10 @@
+package com.aluracursos.screenmatch_frases;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface FraseRepository extends JpaRepository<Frase, Long> {
+
+    @Query("SELECT f FROM Frase f ORDER BY function('RANDOM') LIMIT 1")
+    public Frase obtenerFraseAleatoria();
+}
